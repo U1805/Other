6da6686957bd119337f8b165a39fba7c4b747745
U1805 <45514638+U1805@users.noreply.github.com>
2019-12-21 21:54:48 +0800

    Add files via upload

---
+++ Ero/Login.js
@@ -0,0 +1,11 @@
+function tip(){
+	console.log("6VkSYPP3Jv");
+	console.log("W2X6qZOjyZ");
+}
+function Login(){
+	var name=document.getElementsById("acs").value;
+	var psw =document.getElementsById("pas").value;
+	if (name=="6VkSYPP3Jv" && psw=="W2X6qZOjyZ"){
+	window.location.href="https://u1805.github.io/Ero/";
+	}
+}
